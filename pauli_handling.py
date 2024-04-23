83795238bce12e152399373473bf5d54f4bf9d11
milena-roehrs <167077554+milena-roehrs@users.noreply.github.com>
2024-04-23 13:33:46 +0200

    Add changes pauli_handling.py

--- pauli_handling.py
+++ pauli_handling.py
@@ -1,3 +1,21 @@
+"""
+This file is part of the program BOIS-with-immediate-sharing, which implements the variational quantum eigensolver (VQE) algorithm based on Bayesian optimisation (BO) with different types of information sharing allowing "immediate sharing".
+Copyright (C) 2024  Milena Röhrs
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import copy
 
 def twopaulis(list1, list2):
